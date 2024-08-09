2c1b37a1276d0ae085e0bb6290e47a5e2cd8d741
adaviladev <alvarodaviladev@gmail.com>
2024-08-09 00:46:16 -0400

    add mouse paint functionality

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
+let isDragging = false;
 
-for (let i = 0; i < 16*16; i++) {
+function makeNewGrid(num) {
+  container.innerHTML = "";
+  for (let i = 0; i < num * num; i++) {
     const div = document.createElement("div");
-    container.appendChild(div)
-    div.setAttribute("class", "col")
-    div.textContent = "Hola"
-    div.style.border = "1px solid red"
-    div.style.flex = "1"
+    div.setAttribute("class", "col");
+    div.textContent = "";
+    div.style.border = "1px solid red";
+    div.style.flex = `0 0 calc(100% / ${num})`;
+    div.style.height = `calc(100% / ${num})`;
+    div.style.boxSizing = "border-box";
+    div.style.transition = "background 0.3s ease"
+
+    div.addEventListener("mousedown", function () {
+        isDragging = true;
+      div.style.background = "gray";
+    });
+
+    div.addEventListener("mouseover", function () {
+        if (isDragging) {
+              div.style.background = "gray";    
+        }
+
+    });
+
+    container.appendChild(div);
+  }
 }
 
+container.style.display = "flex";
+container.style.flexWrap = "wrap";
+container.style.width = "100%";
+container.style.height = "100vh";
 
-container.style.display = "flex"
+const makeGridBtn = document.querySelector(".makeGridBtn");
+makeGridBtn.addEventListener("click", function () {
+  const num = parseInt(prompt("How many squares per side do you want."), 10);
+  if (!isNaN(num) && num > 0) {
+    makeNewGrid(num);
+  } else {
+    alert("Please enter a valid number.")
+  }
+});
