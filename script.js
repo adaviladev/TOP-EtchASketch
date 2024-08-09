b4229479223f70d853a7fa681632d00cd71be2fa
adaviladev <alvarodaviladev@gmail.com>
2024-08-09 01:53:34 -0400

    add color randomizer

--- script.js
+++ script.js
@@ -7,26 +7,29 @@ function makeNewGrid(num) {
     const div = document.createElement("div");
     div.setAttribute("class", "col");
     div.textContent = "";
-    div.style.border = "1px solid red";
+    div.style.border = "1px solid black";
     div.style.flex = `0 0 calc(100% / ${num})`;
     div.style.height = `calc(100% / ${num})`;
     div.style.boxSizing = "border-box";
-    div.style.transition = "background 0.3s ease"
+    div.style.transition = "background 0.3s ease";
 
     div.addEventListener("mousedown", function () {
-        isDragging = true;
-      div.style.background = "gray";
+      isDragging = true;
+      div.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
     });
 
     div.addEventListener("mouseover", function () {
-        if (isDragging) {
-              div.style.background = "gray";    
-        }
-
+      if (isDragging) {
+        div.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
+      }
     });
 
     container.appendChild(div);
   }
+
+  document.addEventListener("mouseup", function () {
+    isDragging = false;
+  });
 }
 
 container.style.display = "flex";
@@ -36,10 +39,10 @@ container.style.height = "100vh";
 
 const makeGridBtn = document.querySelector(".makeGridBtn");
 makeGridBtn.addEventListener("click", function () {
-  const num = parseInt(prompt("How many squares per side do you want."), 10);
-  if (!isNaN(num) && num > 0) {
+  const num = parseInt(prompt("How many squares per side do you want? (max 100)"), 10);
+  if (!isNaN(num) && num > 0 && num <= 100) {
     makeNewGrid(num);
   } else {
-    alert("Please enter a valid number.")
+    alert("Please enter a valid number between 1 and 100.");
   }
 });
